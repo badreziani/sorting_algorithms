1263c1ebb16ccc59f13cf073a0673e1d0f168b59
root <root@DESKTOP-PBSSUIU>
2024-04-28 18:38:54 +0100

    Update on task 2

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * of integers in ascending order using
+ * the Insertion sort algorithm
+ *
+ * @list: the list to be sorted
+ *
+ * Return: nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+}
